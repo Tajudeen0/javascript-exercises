f66050781ac9a7b177d354af15b2ba3ab1a09a5e
Adebanjo Timothy <adebanjotimothy3235@gmail.com>
2026-06-23 11:55:02 +0100

    Create a function to repeat a given string the number of times it specifies

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,27 @@
-const repeatString = function() {
+function repeatString(string, num) {
+    
+    if (num > 0) {
+        let stringInput = [];
+        for (let i = num; i > 0; i--) {
+            stringInput.push(string);
+            stringOutput = stringInput.join('');
+    } 
+        return stringOutput;
+    } else if (num == 0) {
+        return ''
+    } else {
+        return 'ERROR'
+    }
 
-};
+}
+
+
+repeatString('hey', 5);
+
+
+//const repeatString = function() {
+
+//};
 
 // Do not edit below this line
 module.exports = repeatString;
