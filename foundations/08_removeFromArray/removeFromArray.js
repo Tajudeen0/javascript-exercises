950824815ca0648d46beb6e32cb936ba3e7483d3
Adebanjo Timothy <adebanjotimothy3235@gmail.com>
2026-06-30 10:28:05 +0100

    Populate a function to remove given array items if present

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...theArgs) {
+  for (const arg of theArgs) {
+    if (array.includes(arg)) {
+    array =  array.filter(n => n !== arg)
+    }
+  }
+  console.log(array);
+  return array;
+}
 
+let arr = [1,2,3,4,5,6]
+
+console.log(removeFromArray(arr, 5, 4, 3, 2, 1, 6))
 // Do not edit below this line
 module.exports = removeFromArray;
