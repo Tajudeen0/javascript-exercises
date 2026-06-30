1bd505918de7e73216fb6ea6d816d315351ce78f
Adebanjo Timothy <adebanjotimothy3235@gmail.com>
2026-06-30 11:27:23 +0100

    Create a function to sum numbers within a range

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    let inputs = args.sort()
+    let i = args[0];
+    let sum = 0;
 
+    if (inputs.every(v => v >= 0) && inputs.every( v => Number.isInteger(v))) {
+        for (i; i <= args[1]; i++) {
+            sum += i;
+        }
+         return sum;
+    } else {
+        return "ERROR";
+    }
+
+
+   
 };
+
+console.log(sumAll(1,5));
 
 // Do not edit below this line
 module.exports = sumAll;
