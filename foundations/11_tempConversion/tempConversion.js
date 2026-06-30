dee5c945d71776619fe192ec63a927abd8c90d5e
Adebanjo Timothy <adebanjotimothy3235@gmail.com>
2026-06-30 12:33:44 +0100

    Create a temperature Calculator

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(arg) {
+  return Math.round(((arg-32) * 5/9)* 10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(arg) {
+  return Math.round((arg * 9/5 + 32) * 10)/10
 };
+
+convertToCelsius(35);
 
 // Do not edit below this line
 module.exports = {
